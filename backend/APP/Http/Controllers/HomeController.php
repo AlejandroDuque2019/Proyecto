ca7fcfbff0e2e9bae0b97c4d21c361c5a200e851
AlejandroDuque2019 <54971750+AlejandroDuque2019@users.noreply.github.com>
2019-11-08 12:02:35 -0500

    Add files via upload

---
+++ backend/APP/Http/Controllers/HomeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('home');
+    }
+
+    public function hola(Request $request){
+      $input = $request->all();
+      $datos = ["nombre"=>$input["nombre"]];
+      return response()->json($datos);
+    }
+}
